c66beda4a0bfd77937d368102781258890bd3b14
GoldenChrysus <artizhay@gmail.com>
2019-07-17 17:15:53 -0400

    Adding FFXIV overlay build.

---
+++ ffxiv-overlay/precache-manifest.bac11e691cab353cd4c0a66ad65f2538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b6925675fc3a8d54d64d0dfdfeb2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96899ae3e8bd6b6748a",
+    "url": "/static/css/main.fe1dda19.chunk.css"
+  },
+  {
+    "revision": "ed7f9eaafe82ecfd29c5",
+    "url": "/static/js/2.910dadc1.chunk.js"
+  },
+  {
+    "revision": "b96899ae3e8bd6b6748a",
+    "url": "/static/js/main.1942b71b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
