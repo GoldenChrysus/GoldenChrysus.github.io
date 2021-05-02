1cdd8fa5c485c098f0f41160169cb56007c11c14
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-05-02 02:41:28 +0000

    Update production build: 0.28.0-alpha

--- ffxiv/ember-overlay/service-worker.js
+++ ffxiv/ember-overlay/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.949b30682758411f843b0bb79ab46594.js"
+  "./precache-manifest.00b1108670ce70fac403d12ee158a049.js"
 );
 
 self.addEventListener('message', (event) => {
